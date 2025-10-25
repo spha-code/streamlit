529aa4c0f5a7c6220c3cb3a6067949dd9b197745
spha-code <hadjidjanos@gmail.com>
2025-10-25 17:58:51 +0200

    csv directory and csv files

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("Hello World!")
+st.write({"Hello World : Hellow World"})
+st.write(123)
+st.write(False)
+
+343 + 232
+
+pressed = st.button("Press me!")
+print(pressed)
+
+if pressed:
+    st.write("Gello")
